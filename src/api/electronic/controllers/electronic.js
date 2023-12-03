f1c25d183a9a7a7a44c4fd3d9455f7889fcde9d9
Ayo <146078449+ayo-bk@users.noreply.github.com>
2023-12-03 19:16:52 +0100

    Add files via upload

---
+++ src/api/electronic/controllers/electronic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * electronic controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::electronic.electronic');
---
+++ src/api/electronic/routes/electronic.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * electronic router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::electronic.electronic');
